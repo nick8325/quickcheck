17b7cf489741b6d74fb3dfa5bc7e88a65aa27fab
Nick Smallbone <nicsma@chalmers.se>
2009-09-16 16:36:05 +0100

    Use extensible exceptions on GHC 6.10.

--- Test/QuickCheck/Exception.hs
+++ Test/QuickCheck/Exception.hs
@@ -1,20 +1,25 @@
+{-# LANGUAGE CPP #-}
 module Test.QuickCheck.Exception where
+
+#if !defined(__GLASGOW_HASKELL__) || (__GLASGOW_HASKELL__ >= 609)
+#define EXCEPTION SomeException
+#else
+#define EXCEPTION Exception
+#endif
 
 import Control.Exception
   ( evaluate
   , try
-  , Exception
+  , EXCEPTION
   )
 
 --------------------------------------------------------------------------
 -- try evaluate
 
---type Exception = SomeException
-
-tryEvaluate :: a -> IO (Either Exception a)
+tryEvaluate :: a -> IO (Either EXCEPTION a)
 tryEvaluate x = tryEvaluateIO (return x)
 
-tryEvaluateIO :: IO a -> IO (Either Exception a)
+tryEvaluateIO :: IO a -> IO (Either EXCEPTION a)
 tryEvaluateIO m = try (m >>= evaluate)
 --tryEvaluateIO m = Right `fmap` m
 
