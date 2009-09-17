d1ee49800142ec07d0c61954f4f06f646e09fb8d
Nick Smallbone <nicsma@chalmers.se>
2009-09-17 17:16:00 +0100

    Make it work from cabal even if you choose the old base library

--- Test/QuickCheck/Exception.hs
+++ Test/QuickCheck/Exception.hs
@@ -1,25 +1,29 @@
 {-# LANGUAGE CPP #-}
 module Test.QuickCheck.Exception where
 
-#if !defined(__GLASGOW_HASKELL__) || (__GLASGOW_HASKELL__ >= 609)
-#define EXCEPTION SomeException
-#else
-#define EXCEPTION Exception
+#if defined(MIN_VERSION_base)
+#if !(MIN_VERSION_base(4,0,0))
+#define SomeException Exception
+#endif
+#endif
+
+#if defined(__GLASGOW_HASKELL__) && (__GLASGOW_HASKELL__ < 609)
+#define SomeException Exception
 #endif
 
 import Control.Exception
   ( evaluate
   , try
-  , EXCEPTION
+  , SomeException
   )
 
 --------------------------------------------------------------------------
 -- try evaluate
 
-tryEvaluate :: a -> IO (Either EXCEPTION a)
+tryEvaluate :: a -> IO (Either SomeException a)
 tryEvaluate x = tryEvaluateIO (return x)
 
-tryEvaluateIO :: IO a -> IO (Either EXCEPTION a)
+tryEvaluateIO :: IO a -> IO (Either SomeException a)
 tryEvaluateIO m = try (m >>= evaluate)
 --tryEvaluateIO m = Right `fmap` m
 
