ba671300bcab846f750ae25e521b557bfd47aac9
Nick Smallbone <nicsma@chalmers.se>
2010-01-12 14:47:04 +0000

    Added isInterrupt to Test.QuickCheck.Exception

--- Test/QuickCheck/Exception.hs
+++ Test/QuickCheck/Exception.hs
@@ -1,31 +1,64 @@
 {-# LANGUAGE CPP #-}
 module Test.QuickCheck.Exception where
 
-#if defined(MIN_VERSION_base)
-#if !(MIN_VERSION_base(4,0,0))
-#define SomeException Exception
-#endif
+#if !defined(__GLASGOW_HASKELL__) || (__GLASGOW_HASKELL__ < 609)
+#define OLD_EXCEPTIONS
 #endif
 
-#if defined(__GLASGOW_HASKELL__) && (__GLASGOW_HASKELL__ < 609)
-#define SomeException Exception
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 607
+#define GHC_INTERRUPT
 #endif
 
-import Control.Exception
-  ( evaluate
-  , try
-  , SomeException
+#if defined OLD_EXCEPTIONS
+import Control.Exception(evaluate, try, Exception(..))
+#else
+import Control.Exception.Extensible(evaluate, try, SomeException(SomeException)
+#if defined(GHC_INTERRUPT)
+  , AsyncException(UserInterrupt)
+#endif
   )
+#endif
+
+#if defined(GHC_INTERRUPT)
+import Panic(GhcException(Interrupted))
+import Data.Typeable
+#if defined(OLD_EXCEPTIONS)
+import Data.Dynamic
+#endif
+#endif
+
+#if defined(OLD_EXCEPTIONS)
+type AnException = Control.Exception.Exception
+#else
+type AnException = SomeException
+#endif
 
 --------------------------------------------------------------------------
 -- try evaluate
 
-tryEvaluate :: a -> IO (Either SomeException a)
+tryEvaluate :: a -> IO (Either AnException a)
 tryEvaluate x = tryEvaluateIO (return x)
 
-tryEvaluateIO :: IO a -> IO (Either SomeException a)
+tryEvaluateIO :: IO a -> IO (Either AnException a)
 tryEvaluateIO m = try (m >>= evaluate)
 --tryEvaluateIO m = Right `fmap` m
+
+-- Test if an exception was a ^C.
+-- QuickCheck won't try to shrink an interrupted test case.
+isInterrupt :: AnException -> Bool
+
+#if defined(GHC_INTERRUPT)
+#if defined(OLD_EXCEPTIONS)
+isInterrupt (DynException e) = fromDynamic e == Just Interrupted
+isInterrupt _ = False
+#else
+isInterrupt (SomeException e) =
+  cast e == Just Interrupted || cast e == Just UserInterrupt
+#endif
+
+#else /* !defined(GHC_INTERRUPT) */
+isInterrupt _ = False
+#endif
 
 --------------------------------------------------------------------------
 -- the end.
