77e0d08f1c84afa2573b32dbe64acd083f840ac3
Nick Smallbone <nicsma@chalmers.se>
2010-08-31 02:51:14 +0100

    More weird examples

--- examples/ExWeird.hs
+++ examples/ExWeird.hs
@@ -16,12 +16,14 @@ prop2 (Fun _ f :: Fun Int Int) (Fun _ g :: Fun Int Int) x = f (g x) == g (f x)
 
 fibs = 0:1:zipWith (+) fibs (tail fibs)
 
-prop3 n = n >= 0 && n <= 100000 ==> 10000000 `within` (fibs!!(n+50000) + fibs!!(n+50001) == fibs!!(n+50002))
+prop3 n = n >= 0 && n <= 100000 ==> 100000 `within` (fibs!!(n+50000) + fibs!!(n+50001) == fibs!!(n+50002))
 
 revrev (xs :: [Int]) = within 1000 (reverse (reverse xs) == xs)
 
 undef (n :: Int) = undefined :: Bool
 undef2 (n :: Int) = undefined :: Property
+undef25 (n :: Int) = return undefined :: Property
+undef21 (n :: Int) = return (MkProp (MkRose undefined [])) :: Property
 undef3 = undefined :: Property
 undef4 = collect "" (undefined :: Property)
 undef5 = collect (undefined :: String) (undefined :: Property)
