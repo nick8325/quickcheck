c4f8f22139f97792096895664310b4d638a2bd8e
Koen Claessen <koen@chalmers.se>
2010-10-01 15:03:24 +0100

    cleaned up merge example, changed name

--- examples/Merge.hs
+++ examples/Merge.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS -fglasgow-exts #-}
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell #-}
 module Main where
 
 --------------------------------------------------------------------------
