dc37e8a86cb58ec72f2ebae2d8669c40435ceb0d
RyanGlScott <ryan.gl.scott@ku.edu>
2015-01-03 22:35:04 -0600

    Allow single star-kinded TyVarBndrs in quickCheckAll

--- Test/QuickCheck/All.hs
+++ Test/QuickCheck/All.hs
@@ -79,8 +79,13 @@ infoType (VarI _ ty _ _) = ty
 
 deconstructType :: Error -> Type -> Q ([Name], Cxt, Type)
 deconstructType err ty0@(ForallT xs ctx ty) = do
-  let plain (PlainTV _) = True
-      plain _ = False
+  let plain (PlainTV  _)       = True
+#if MIN_VERSION_template_haskell(2,8,0)
+      plain (KindedTV _ StarT) = True
+#else
+      plain (KindedTV _ StarK) = True
+#endif
+      plain _                  = False
   unless (all plain xs) $ err "Higher-kinded type variables in type"
   return (map (\(PlainTV x) -> x) xs, ctx, ty)
 deconstructType _ ty = return ([], [], ty)
