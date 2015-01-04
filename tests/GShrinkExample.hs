5e3c96d35b37daa5aff8141c537cdb5367d58981
Niklas Hambüchen <mail@nh2.me>
2015-01-04 15:00:37 +0100

    tests: Add `genericShrink` example

---
+++ tests/GShrinkExample.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE DeriveGeneric, ScopedTypeVariables #-}
+
+module GCoShrinkExample where
+
+import GHC.Generics (Generic)
+import Test.QuickCheck
+
+data Nat = Z | S Nat deriving (Eq, Show, Generic)
+
+
+instance Arbitrary Nat
+
+
+main :: IO ()
+main = do
+  print $ genericShrink (S (S Z)) == [S Z]
+  print $ genericShrink [0::Int] == [[]]
