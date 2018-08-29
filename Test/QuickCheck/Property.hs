0c43763ffb6642d46b90428544c907930e0da88f
Nick Smallbone <nick@smallbone.se>
2018-08-29 15:40:28 +0200

    Bump default certainty.

--- Test/QuickCheck/Property.hs
+++ Test/QuickCheck/Property.hs
@@ -428,7 +428,7 @@ checkCoverageWith confidence =
 stdConfidence :: Confidence
 stdConfidence =
   Confidence {
-    certainty = 10^6,
+    certainty = 10^8,
     tolerance = 0.9 }
 
 -- | Attaches a label to a property. This is used for reporting
