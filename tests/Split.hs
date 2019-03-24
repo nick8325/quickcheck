8a5b0a3aea9beb76cb37ecfb69324c42e148e65d
Nick Smallbone <nick@smallbone.se>
2019-03-24 21:26:47 +0100

    Fix comment.

--- tests/Split.hs
+++ tests/Split.hs
@@ -2,15 +2,15 @@ import Test.QuickCheck
 import Test.QuickCheck.Random
 import Data.List
 
--- This type allows us to run splitInteger and get a list of bits out.
+-- This type allows us to run integerVariant and get a list of bits out.
 newtype Splits = Splits { unSplits :: [Bool] } deriving (Eq, Ord, Show)
 
 instance Splittable Splits where
   left (Splits xs) = Splits (xs ++ [False])
   right (Splits xs) = Splits (xs ++ [True])
 
--- Check that splitInteger gives a prefix-free code,
--- i.e., if m /= n then splitInteger m is not a prefix of splitInteger n.
+-- Check that integerVariant gives a prefix-free code,
+-- i.e., if m /= n then integerVariant m is not a prefix of integerVariant n.
 prop_split_prefix :: Property
 prop_split_prefix =
   once $ forAllShrink (return [-10000..10000]) shrink $ \ns ->
