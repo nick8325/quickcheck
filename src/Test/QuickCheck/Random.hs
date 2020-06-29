90b8b0dcdcc12e0bcfa13b97acfcbc8ca49e50f7
Nick Smallbone <nick@smallbone.se>
2020-06-29 19:53:59 +0200

    More work on supporting both old and new random

--- src/Test/QuickCheck/Random.hs
+++ src/Test/QuickCheck/Random.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_HADDOCK hide #-}
+{-# OPTIONS_GHC -Wno-deprecations #-}
 -- | A wrapper around the system random number generator. Internal QuickCheck module.
 {-# LANGUAGE CPP #-}
 #ifndef NO_SAFE_HASKELL
@@ -7,72 +8,81 @@
 module Test.QuickCheck.Random where
 
 import System.Random
-#ifndef NO_SPLITMIX
+#ifdef SPLITMIX
 import System.Random.SplitMix
 #endif
-import Data.Bits
-#if MIN_VERSION_random(1,2,0)
-import Data.Coerce
+#if !defined(OLD_RANDOM)
+import System.Random.Internal
 #endif
+import Data.Bits
 
 -- | The "standard" QuickCheck random number generator.
 -- A wrapper around either 'SMGen' on GHC, or 'StdGen'
 -- on other Haskell systems.
-#ifdef NO_SPLITMIX
-newtype QCGen = QCGen StdGen
-#else
+#ifdef SPLITMIX
 newtype QCGen = QCGen SMGen
+#else
+newtype QCGen = QCGen StdGen
 #endif
 
 instance Show QCGen where
   showsPrec n (QCGen g) s = showsPrec n g s
 instance Read QCGen where
+#if defined(OLD_RANDOM)
   readsPrec n xs = [(QCGen g, ys) | (g, ys) <- readsPrec n xs]
+#else
+  readsPrec n xs = [(QCGen (StdGen g), ys) | (g, ys) <- readsPrec n xs]
+#endif
 
 instance RandomGen QCGen where
-#ifdef NO_SPLITMIX
-  split (QCGen g) =
-    case split g of
-      (g1, g2) -> (QCGen g1, QCGen g2)
-
-  genRange (QCGen g) = genRange g
-  next (QCGen g) =
-    case next g of
-      (x, g') -> (x, QCGen g')
-#else
+#ifdef SPLITMIX
   split (QCGen g) =
     case splitSMGen g of
       (g1, g2) -> (QCGen g1, QCGen g2)
-
-#if MIN_VERSION_random(1,2,0)
-  genWord8 (QCGen g) = coerce (genWord8 g)
-  genWord16 (QCGen g) = coerce (genWord16 g)
-  genWord32 (QCGen g) = coerce (genWord32 g)
-  genWord64 (QCGen g) = coerce (genWord64 g)
-  genWord32R r (QCGen g) = coerce (genWord32R r g)
-  genWord64R r (QCGen g) = coerce (genWord64R r g)
-  genShortByteString n (QCGen g) = coerce (genShortByteString n g)
-#else
-  genRange (QCGen g) = (minBound, maxBound)
-
+  genRange _ = (minBound, maxBound)
   next (QCGen g) =
     case nextInt g of
       (x, g') -> (x, QCGen g')
+#else
+  split (QCGen g) =
+    case split g of
+      (g1, g2) -> (QCGen g1, QCGen g2)
+  genRange (QCGen g) = genRange g
+  next = wrapQCGen next
+
+#ifndef OLD_RANDOM
+  genWord8 = wrapQCGen genWord8
+  genWord16 = wrapQCGen genWord16
+  genWord32 = wrapQCGen genWord32
+  genWord64 = wrapQCGen genWord64
+  genWord32R r = wrapQCGen (genWord32R r)
+  genWord64R r = wrapQCGen (genWord64R r)
+  genShortByteString n = wrapQCGen (genShortByteString n)
 #endif
 #endif
 
-newQCGen :: IO QCGen
-#ifdef NO_SPLITMIX
-newQCGen = fmap QCGen newStdGen
+{-# INLINE wrapQCGen #-}
+#ifdef SPLITMIX
+wrapQCGen :: (SMGen -> (a, SMGen)) -> (QCGen -> (a, QCGen))
 #else
+wrapQCGen :: (StdGen -> (a, StdGen)) -> (QCGen -> (a, QCGen))
+#endif
+wrapQCGen f (QCGen g) =
+  case f g of
+    (x, g') -> (x, QCGen g')
+
+newQCGen :: IO QCGen
+#ifdef SPLITMIX
 newQCGen = fmap QCGen newSMGen
+#else
+newQCGen = fmap QCGen newStdGen
 #endif
 
 mkQCGen :: Int -> QCGen
-#ifdef NO_SPLITMIX
-mkQCGen n = QCGen (mkStdGen n)
-#else
+#ifdef SPLITMIX
 mkQCGen n = QCGen (mkSMGen (fromIntegral n))
+#else
+mkQCGen n = QCGen (mkStdGen n)
 #endif
 
 -- Parameterised in order to make this code testable.
