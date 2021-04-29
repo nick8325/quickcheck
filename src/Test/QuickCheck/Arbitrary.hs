3a35caac75dbd8cb3048003fa885184c117b12da
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-04-29 07:38:19 +0200

    Make NonEmpty an instance of Arbitrary1

--- src/Test/QuickCheck/Arbitrary.hs
+++ src/Test/QuickCheck/Arbitrary.hs
@@ -88,6 +88,7 @@ module Test.QuickCheck.Arbitrary
 
 import Control.Applicative
 import Data.Foldable(toList)
+import Data.List.NonEmpty(NonEmpty((:|)))
 import System.Random(Random)
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Random
@@ -484,6 +485,19 @@ shrinkList shr xs = concat [ removes k n xs | k <- takeWhile (>0) (iterate (`div
                ++ [ x:xs' | xs' <- shrink xs ]
                ++ [ x':xs | x'  <- shrink x ]
 -}
+
+instance Arbitrary1 NonEmpty where
+  liftArbitrary arb = (:|) <$> arb <*> listOf arb
+
+  liftShrink shr (l :| ls) = ((:| ls) <$> shr l) ++ go
+    where go | (y:ys) <- ls = (y :| ys) : tl
+             | otherwise = tl
+          tl = (l :|) <$> shrinkList shr ls
+
+instance Arbitrary a => Arbitrary (NonEmpty a) where
+  arbitrary = arbitrary1
+  shrink = shrink1
+
 
 instance Integral a => Arbitrary (Ratio a) where
   arbitrary = arbitrarySizedFractional
