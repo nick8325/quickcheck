d6e2a40f8f0f8419a10228d3e61d693d338d33b4
L0neGamer <benjaminbwm@gmail.com>
2026-03-02 23:17:29 +0000

    add old-time instances for ghc
    many errors when building old-locale, see below for example
    ```
    error: Time.hsc:380:2: error: #error "Don't know how to get at timezone name on your OS."
    ```

--- src/Test/QuickCheck/Arbitrary.hs
+++ src/Test/QuickCheck/Arbitrary.hs
@@ -210,6 +210,9 @@ import GHC.Generics
 #ifndef NO_HASHABLE
 import Data.Hashable (Hashable, hashed, hashedHash, Hashed)
 #endif
+#ifndef NO_OLDTIME
+import qualified System.Time as OldTime
+#endif
 #ifndef NO_SEMIGROUP
 import qualified Data.Semigroup as Semigroup
 #endif
@@ -266,6 +269,7 @@ a   CChar, CSChar, CUChar, CShort, CUShort, CInt, CUInt, CLong, CULong, CPtrdiff
 if c type constructors allowed: a CClock, CTime
   if foreign c unsigned seconds: a CUSeconds, CSUSeconds
 a   CFloat, CDouble
+if old-time allowed: ac OldTime.Month, OldTime.Day, OldTime.ClockTime, OldTime.TimeDiff, OldTime.CalendarTime
 if hashable allowed: ac Hashed
 a   Set
 a1  Map
@@ -1025,6 +1029,77 @@ instance Arbitrary CFloat where
 instance Arbitrary CDouble where
   arbitrary = arbitrarySizedFractional
   shrink = shrinkDecimal
+
+#ifndef NO_OLDTIME
+instance Arbitrary OldTime.Month where
+    arbitrary = arbitraryBoundedEnum
+
+instance CoArbitrary OldTime.Month where
+    coarbitrary = coarbitraryEnum
+
+instance Arbitrary OldTime.Day where
+    arbitrary = arbitraryBoundedEnum
+
+instance CoArbitrary OldTime.Day where
+    coarbitrary = coarbitraryEnum
+
+instance Arbitrary OldTime.ClockTime where
+    arbitrary =
+        OldTime.TOD <$> choose (0, fromIntegral (maxBound :: Int32))
+                    <*> choose (0, 1000000000000 - 1)
+    shrink (OldTime.TOD s p) =
+        [ OldTime.TOD s' p  | s' <- shrink s ] ++
+        [ OldTime.TOD s  p' | p' <- shrink p ]
+
+instance CoArbitrary OldTime.ClockTime where
+    coarbitrary (OldTime.TOD s p) =
+        coarbitrary s . coarbitrary p
+
+instance Arbitrary OldTime.TimeDiff where
+    -- a bit of a cheat ...
+    arbitrary =
+        OldTime.normalizeTimeDiff <$>
+           (OldTime.diffClockTimes <$> arbitrary <*> arbitrary)
+    shrink td@(OldTime.TimeDiff year month day hour minute sec picosec) =
+        [ td { OldTime.tdYear    = y' } | y' <- shrink year    ] ++
+        [ td { OldTime.tdMonth   = m' } | m' <- shrink month   ] ++
+        [ td { OldTime.tdDay     = d' } | d' <- shrink day     ] ++
+        [ td { OldTime.tdHour    = h' } | h' <- shrink hour    ] ++
+        [ td { OldTime.tdMin     = m' } | m' <- shrink minute  ] ++
+        [ td { OldTime.tdSec     = s' } | s' <- shrink sec     ] ++
+        [ td { OldTime.tdPicosec = p' } | p' <- shrink picosec ]
+
+instance CoArbitrary OldTime.TimeDiff where
+    coarbitrary (OldTime.TimeDiff year month day hour minute sec picosec) =
+        coarbitrary year    .
+        coarbitrary month   .
+        coarbitrary day     .
+        coarbitrary hour    .
+        coarbitrary minute  .
+        coarbitrary sec     .
+        coarbitrary picosec
+
+-- UTC only
+instance Arbitrary OldTime.CalendarTime where
+    arbitrary = OldTime.toUTCTime <$> arbitrary
+
+instance CoArbitrary OldTime.CalendarTime where
+    coarbitrary (OldTime.CalendarTime
+                        year month day hour minute sec picosec
+                        wDay yDay tzName tz isDST) =
+        coarbitrary year    .
+        coarbitrary month   .
+        coarbitrary day     .
+        coarbitrary hour    .
+        coarbitrary minute  .
+        coarbitrary sec     .
+        coarbitrary picosec .
+        coarbitrary wDay    .
+        coarbitrary yDay    .
+        coarbitrary tzName  .
+        coarbitrary tz      .
+        coarbitrary isDST
+#endif
 
 -- Arbitrary instances for container types
 
